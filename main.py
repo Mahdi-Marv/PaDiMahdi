d5fd852e13fd7d15cba3116a611b600a5893d399
Mahdi-Marv <mahdihajialilue@gmail.com>
2024-03-24 13:12:07 +0330

    plots

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ def main():
     test_data.to_csv('test_dataset.csv', index=False)
 
     # Use the CustomDataset class
-    train_dataset = WBC_dataset(csv_path='train_dataset.csv', image_path='wbc/segmentation_WBC-master/Dataset 1')
+    # train_dataset = WBC_dataset(csv_path='train_dataset.csv', image_path='wbc/segmentation_WBC-master/Dataset 1')
     test_dataset = WBC_dataset(csv_path='test_dataset.csv', image_path='wbc/segmentation_WBC-master/Dataset 1', phase='test')
 
     # Create DataLoaders
